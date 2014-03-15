0b7a48c2aa150e2b6f0ef432d1d12781edbd818e
zacharilius <zabensley@gmail.com>
2014-03-15 10:08:32 -0400

    Finished & Debugged GF1

--- GF1/GF1.java
+++ GF1/GF1.java
@@ -8,9 +8,14 @@ import java.util.logging.Logger;
 import java.util.ArrayList;
 
 /**
- * 
+ * 	GF1 Algorithm
+ *	
+ *  @author Zachary Bensley & Liangliang Xiao(EEA Algorithm)
  */
 public class GF1 {
+	/**
+	 * The main method of the GF1 class
+	 */
 	public static void main(String[] args){		
 		//Input Files
 		int[] ia = inputFile("input.txt");
@@ -20,9 +25,16 @@ public class GF1 {
 		outputText += subtractMod(ia[1], ia[2], ia[0]) + "\n";
 		outputText += multiplyMod(ia[1], ia[2], ia[0]) + "\n";
 		outputText += divideMod(ia[1], ia[2], ia[0]);
-		
+				
 		outputFile(outputText);
 	}
+	/**
+	 * Inputs the selected file and stores it in an integer array
+	 *
+	 * @param	String	fileName	The name of the file to be inputted.
+	 *
+	 * @return	int[]	Returns an integer array with the contents of the selected file
+	 */
 	public static int[] inputFile(String fileName){
 		int[] ia = new int[3];
 		String line = "";
@@ -43,6 +55,16 @@ public class GF1 {
         
         
 	}
+	/**
+	*  This method adds the x integer and the y integer and then performs modulo with
+	*  the mod variable mod.
+	*
+	* @param int 	x	A number to be added
+	* @param int	y	A number to be added
+	* @param int	mod	The modulo value 
+	*
+	* @return int	Returns the result
+	*/ 
 	public static int addMod(int x, int y, int mod){
 		int r = (x + y) % mod;
 		if(r < 0){
@@ -50,6 +72,16 @@ public class GF1 {
 		}
 		return r;
 	}
+	/**
+	*  This method subtracts the y integer from the x integer and then performs modulo with
+	*  the mod variable mod.
+	*
+	* @param int 	x	The number being subtracted from 
+	* @param int	y	The number subtracting from x
+	* @param int	mod	The modulo value 
+	*
+	* @return int	Returns the result
+	*/ 
 	public static int subtractMod(int x, int y, int mod){
 		int r = (x - y) % mod;
 		if(r < 0){
@@ -57,6 +89,15 @@ public class GF1 {
 		}
 		return r;
 	}
+	/**  This method multiplies the x integer and the y integer and then performs modulo with
+	*  the mod variable mod.
+	*
+	* @param int 	x	A number to be multiplied
+	* @param int	y	A number to be multiplied
+	* @param int	mod	The modulo value 
+	*
+	* @return int	Returns the result
+	*/ 
 	public static int multiplyMod(int x, int y, int mod){
 		int r = (x * y) % mod;
 		if(r < 0){
@@ -64,7 +105,22 @@ public class GF1 {
 		}
 		return r;
 	}
+	/**
+	*  This method divides the x integer by the y integer and then performs modulo with
+	*  the mod variable mod. If y == 0, it throws an Arithmetic Exception.
+	*
+	* @param int 	x	The numerator
+	* @param int	y	The denominator
+	* @param int	mod	The modulo value 
+	*
+	* @return int	Returns the result
+	*
+	* @throw 	ArithmeticException	If the y is 0. Throws because cannot divide by 0.
+	*/ 
 	public static int divideMod(int x, int y, int mod){
+		if(y == 0){
+			throw new ArithmeticException("ERROR: Cannot divide by Zero");
+		}
 		return (x * EEA(y, mod)[0]) % mod;
 	}	
 	/** 
@@ -84,7 +140,12 @@ public class GF1 {
             return new int[]{R[1], R[0]-q*R[1]};
         }
     }
-public static void outputFile(String outputString){
+	/**
+	*  This method outputs the specified string to output.txt file in the same folder 
+	*
+	* @param String outputString The string to be saved to output.txt
+	*/    
+	public static void outputFile(String outputString){
         PrintWriter out = null;
         try {
             out = new PrintWriter("output.txt");
